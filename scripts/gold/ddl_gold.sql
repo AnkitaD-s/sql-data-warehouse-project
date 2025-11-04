b39c2cb9c0569a68df34a83c444b5d640c5ab619
Ankita Das <addy.wizard@gmail.com>
2025-11-04 19:53:38 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,63 @@
+-- View: gold.dim_customers
+-- DROP VIEW gold.dim_customers;
+
+CREATE OR REPLACE VIEW gold.dim_customers
+ AS
+ SELECT row_number() OVER (ORDER BY ci.cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS firstname,
+    ci.cst_lastname AS lastname,
+    la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
+        CASE
+            WHEN ci.cst_gndr::text <> 'n/a'::text THEN ci.cst_gndr
+            ELSE COALESCE(ca.gen, 'n/a'::character varying)
+        END AS gender,
+    ca.bdate AS birthdate,
+    ci.cst_create_date AS create_date
+   FROM silver.crm_cust_info ci
+     LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key::text = ca.cid::text
+     LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key::text = la.cid::text;
+--------------------------------------------------------------------------------------
+-- View: gold.dim_products
+-- DROP VIEW gold.dim_products;
+
+CREATE OR REPLACE VIEW gold.dim_products
+ AS
+ SELECT row_number() OVER (ORDER BY pi.prd_start_dt, pi.prd_id) AS product_key,
+    pi.prd_id AS product_id,
+    pi.prd_key AS product_number,
+    pi.prd_nm AS product_name,
+    pi.prd_line AS product_line,
+    pi.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+    pi.prd_cost AS product_cost,
+    pi.prd_start_dt AS start_date
+   FROM silver.crm_prd_info pi
+     LEFT JOIN silver.erp_px_cat_g1v2 pc ON pi.cat_id::text = pc.id::text
+  WHERE pi.prd_end_dt IS NULL;
+--------------------------------------------------------------------------------------
+-- View: gold.fact_sales
+
+-- DROP VIEW gold.fact_sales;
+
+CREATE OR REPLACE VIEW gold.fact_sales
+ AS
+ SELECT sd.sls_ord_num AS order_number,
+    dp.product_key,
+    dc.customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt AS ship_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales,
+    sd.sls_quantity AS quantity,
+    sd.sls_price AS price
+   FROM silver.crm_sales_details sd
+     LEFT JOIN gold.dim_products dp ON sd.sls_prd_key::text = dp.product_number::text
+     LEFT JOIN gold.dim_customers dc ON sd.sls_cust_id = dc.customer_id;
+
+
 
